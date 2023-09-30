f7e608c7b25fdd43f949fab84d099be776110a48
gfavale1 <favalegiacomo25@outlook.com>
2023-09-30 13:43:59 +0200

    Ciao

--- src/main/java/org/example/Calcolatrice.java
+++ src/main/java/org/example/Calcolatrice.java
@@ -10,12 +10,12 @@ public class Calcolatrice {
     public double sum(double a, double b){
         return a + b;
     }
-
     public double multiplication(double a, double b){
         return a * b;
     }
-
-
+    public double substraction(double a, double b) { return a - b;}
+    public double division(double a, double b){ if(b != 0) {return a / b;} else return 0;}
+    public double PD(double a, double b){return b;}
 
     private double a, b;
 }
